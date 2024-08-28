71ad713ea8a009f2fa6f6efebdd898ae8c534e13
fedeocode <federicoovejero@gmail.com>
2024-08-28 12:00:31 -0300

    calculos de faltas

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -4,12 +4,32 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Esta es la clase 7");
+        System.out.printf("  Esta es la clase 7" );
+
+        //Escribir una función para calcular la cantidad de faltas disponibles de acuerdo a un porcentaje de asistencia y cantidad de encuentros determinado
+        //¿Cuál es el resultado para una cursada de 23 encuentros que requiere un 75% de asistencia? ¿Y para el caso de este módulo?
+
+      calcularfaltas(48,80);
+
 
 
 
 
     }
+     public static Integer calcularfaltas(Integer encuentros,Integer porcentaje){
 
+        Integer cantidadfaltas=encuentros-encuentros*porcentaje/100;
+
+         System.out.println("  cantidad de faltas  "  +   cantidadfaltas);
+
+
+
+return cantidadfaltas;
+
+
+
+
+
+     }
 
 }
