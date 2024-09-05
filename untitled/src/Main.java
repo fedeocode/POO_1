c77b9a78ba79c27c23ddc5c434fc8681e55edf69
fedeocode <federicoovejero73@gmail.com>
2024-09-05 11:57:19 -0300

    Clase objeto intanciando,metodos,constructor

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,3 +1,5 @@
+import entidades.Producto;
+
 import java.time.LocalDate;
 import java.util.Arrays;
 
@@ -5,98 +7,19 @@ import java.util.Arrays;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
-        //1. Crea un arrays de cuatro películas e imprimirlas por orden alfabético.
-        String[] peliculas = new String[4];
-        peliculas[0]="batman";
-        peliculas[1]="superman";
-        peliculas[2]="gladiador";
-        peliculas[3]="el señor de los anillos romulo";
-        Arrays.sort(peliculas);
-        for(int i=0; i<peliculas.length; i++){
-            System.out.println("  peliculas  "+ peliculas[i]);
-        }
+/*Instanciando un objeto de la clase producto en el paquete */
 
 
 
+        Producto motoUno=new Producto("g22",20.55,90.000,30);
+
+        System.out.println("Motos disponibles "+'\n'+motoUno.stockDisponible());
+        System.out.println("ganacias motos"+'\n'+motoUno.calcularGanancia());
 
 
 
-
-
-        //crea una función que de acuerdo a la edad del
-        //usuario( recibe fecha de nacimiento) y si tiene entrada indique
-        //si puede o no asistir al evento, debe ser mayor.
-
-        System.out.println(numtexto("100", "50"));
-
-
-        LocalDate fechadenacimiento=LocalDate.of(2024,7,6);
-       System.out.println(ingresa(fechadenacimiento,true));
-
-        cantidad_s("eL SOL SALE POR LAS MAÑANAS");
-
-
-    }
-
-    //Necesitamos crear una función que reciba dos números en
-    //formato texto y retorne el cociente de dividir el primero
-    //por el segundo.
-
-
-        public static Integer numtexto(String num1text,String num2text) {
-            Integer num1= Integer.parseInt(num1text);
-            Integer num2= Integer.parseInt(num2text);
-
-            return num1 / num2;
-
-        }
-
-       //  Escribi una funcion que reciba una cadena de texto y cuente la cantidad de "sss" .retorna la
-    //    cantidad encontrada.
-
-
-
-
-
-   public static int cantidad_s(String cantidads ) {
-
-        int contador=0;
-
-        for(int i=0; i<cantidads.length(); i++){
-
-            if (cantidads.toLowerCase().charAt(i) =='s'){
-
-               contador=contador+1;
-                }
-
-
-
-        }
-        System.out.println("    cantidad de sss   "   +contador);
-    return contador;
-
-
-    }
-
-
-
-
-
-
-    public static Boolean ingresa(LocalDate fechanacimiento, Boolean entrada){
-
-         int edad=LocalDate.now().getYear()-fechanacimiento.getYear();
-           if (entrada && edad>=18){
-
-               return true;
-           }
-         else{
-
-             return false;
-
-           }
 
     }
 
 }
+
---
+++ untitled/src/entidades/Producto.java
@@ -0,0 +1,49 @@
+package entidades;
+
+public class Producto {
+    //Necesitamos diseñar un sistema que permita a un vendedor consultar por un
+    //producto. Los productos tienen nombre,precio de compra, precio de venta y
+    //stock disponibles. Se espera que los
+    //productos puedan informar si hay stock
+    //disponible y cuál es la ganancia
+    //obtenida por cada venta, según los
+    //precios disponibles.>
+    private String nombre;
+    private Double precioCompra;
+    private Double precioVenta;
+    private Integer stockDisponible;
+
+
+    /*Constructor*/
+    public Producto(String nombre, Double precioCompra, Double precioVenta,Integer stockDisponible) {
+      this.nombre=nombre;
+      this.precioCompra=precioCompra;
+      this.precioVenta=precioVenta;
+      this.stockDisponible=stockDisponible;
+
+    }
+
+    public Integer stockDisponible(){
+      return this.stockDisponible;
+
+
+
+    }
+
+    public Double calcularGanancia(){
+
+       return this.precioVenta-this.precioCompra;
+
+    }
+
+
+
+
+
+
+
+
+}
+
+
+
