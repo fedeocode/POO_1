0aae62152119649e054fe276c4a9cf5c7f320e54
fedeocode <federicoovejero73@gmail.com>
2024-10-15 11:40:30 -0300

    Ejercicio Clase 20

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,15 +1,53 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.ArrayList;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("subiendo cambios");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+       agregarMascota();
+
+
+        Mascota perro=new Mascota("doberman","liLY",20);
+    }
+      public static void agregarMascota(){
+
+        boolean continuar = true;
+
+
+        ArrayList<Mascota> mascotas= new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+
+
+        while (continuar) {
+            System.out.println("Ingresa De esta forma:Lili,3,Perro");
+            String ingresaMascota = scanner.nextLine();
+            //scanner.nextLine();
+            String[] arrayinfo = ingresaMascota.split(",");
+            String nombre=arrayinfo[0];
+            String especie=arrayinfo[2];
+            Integer edad=Integer.parseInt(arrayinfo[1]);
+            Mascota mascota = new Mascota(especie,nombre,edad);
+            mascotas.add(mascota);
+// falta esto: sout ("desea continuar?" )-- el espacio vacío es que //está esperando un booleano entiendo
+            System.out.println("¿Continuar ingresando info?");
+
+//System.out.println("¿Continuar ingresando info? ");
+
+           continuar = scanner.nextBoolean();
+            scanner.nextLine();
+
+       }
+
+        for (int i = 0; i < mascotas.size(); i++) {
+
+
+            System.out.println("Mascota ingresada  " + mascotas.get(i));
         }
     }
-}
+
+
+
+
+
+            }
---
+++ untitled/src/Mascota.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class Mascota {
+    private String nombre;
+    private Integer edad;
+    private String especie;
+
+
+    public Mascota(String especie, String nombre, Integer edad) {
+
+        this.edad = edad;
+        this.especie = especie;
+        this.nombre = nombre;
+
+
+
+
+    }
+}
