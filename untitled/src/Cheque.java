5d7c353a5d165227edc9c7d96cc2f1ff554a8856
fedeocode <federicoovejero73@gmail.com>
2024-10-21 22:08:20 -0300

    Sistema de Bancario 1.3

--- untitled/src/Cheque.java
+++ untitled/src/Cheque.java
@@ -1,2 +1,27 @@
+import java.time.LocalDate;
+
 public class Cheque {
+
+    private Double monto;
+    private String bancoEmisor;
+    private LocalDate fechaDeEmision;
+
+    public Cheque(Double monto, String bancoEmisor, LocalDate fechaDeEmision) {
+        this.monto = monto;
+        this.bancoEmisor = bancoEmisor;
+        this.fechaDeEmision = fechaDeEmision;
+    }
+
+   public String mostrarInfo(){
+
+
+        System.out.println("cheque depositado"+this.monto);
+
+        return mostrarInfo();
+
+
+
+   }
+
+
 }
